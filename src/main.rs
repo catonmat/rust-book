7c6a0eb5eefc05d5cebf6fbf33f5b6192ae83064
richard jarram <rjarram@me.com>
2020-02-26 14:28:35 -0500

    added loop

--- src/main.rs
+++ src/main.rs
@@ -9,25 +9,38 @@ fn main() {
 
     // println!("The secret_number is: {}", secret_number);
 
-    println!("Please input your guess");
+    loop {
+        println!("Please input your guess");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    // convert user input into unsigned 32-bit number
-    // simultaneously, we shadow `guess`, converting it from a string to a integer type
-    // `trim()` removes carriage returns \n from user input
-    // `:` annotates the variable tpye, meaning we define guess's type as a unsigned 32 bit number
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        // convert user input into unsigned 32-bit number
+        // simultaneously, we shadow `guess`, converting it from a string to a integer type
+        // `trim()` removes carriage returns \n from user input
+        // `:` annotates the variable tpye, meaning we define guess's type as a unsigned 32 bit number
+        let guess: u32 = match guess.trim().parse() {
+            // parse returns enum `exepect` with 2 variants OK and Err
+            Ok(num) => num,
+            Err(_) => {
+                // `_` matches any value
+                println!("Please enter a valid number...");
+                continue;
+            }
+        };
 
-    println!("You guessed: {}", guess);
+        println!("You guessed: {}", guess);
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small"),
-        Ordering::Greater => println!("Too big"),
-        Ordering::Equal => println!("Congrats! You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("Congrats! You win!");
+                break;
+            }
+        }
     }
 }
